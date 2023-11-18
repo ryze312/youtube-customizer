a8e88f73bb56fe728b587a20302bf1be5be466f7
Ryze <50497128+ryze312@users.noreply.github.com>
2023-11-18 17:54:14 +0300

    Remove outdated TODO (i forgor)

--- src/scripts/page/ytCustomizer.ts
+++ src/scripts/page/ytCustomizer.ts
@@ -2,8 +2,6 @@ import setupPageDetour from "./page.js";
 import setupGuideDetour from "./guide.js";
 import setupFF2MpvDetour from "./ff2mpv.js";
 
-// TODO: Do the general review and clean up before release
-
 {
     const app = document.getElementsByTagName("ytd-app")[0] as YTApp; 
     const pageManager = document.getElementById("page-manager") as YTPageManager;
